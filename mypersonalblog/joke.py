32d9c696ae5a3f0d3438539f6fedf6cfd8f834d4
Administrator <642252108@qq.com>
2018-04-21 17:28:50 +0800

    退出及博客详情页

---
+++ mypersonalblog/joke.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class Joke(object):
+    def __init__(self):
+        self.url = 'https://www.qiushibaike.com/'
+
+    def get_joke(self):
+        res = requests.get(self.url)
+        html = res.text
+        soup = BeautifulSoup(html, 'lxml')
+        content = soup.select('.article > a > .content > span')
+        content_lis = []
+        for i in content:
+            # print(i)
+            j = i.get_text()
+            content_lis.append(j)
+        return content_lis
